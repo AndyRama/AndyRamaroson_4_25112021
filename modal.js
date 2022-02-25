4a1f0c2894e84b5eb1844b242b967c5424a29e6f
AndyRama <andyramaroson@gmail.com>
2022-02-25 16:51:21 +0100

    [enhancement] close modal X

--- modal.js
+++ modal.js
@@ -11,6 +11,7 @@ function editNav() {
 const modalbg = document.querySelector(".bground");
 const modalBtn = document.querySelectorAll(".modal-btn");
 const formData = document.querySelectorAll(".formData");
+const closeModal = document.querySelectorAll(".close");
 
 // launch modal event
 modalBtn.forEach((btn) => btn.addEventListener("click", launchModal));
@@ -19,3 +20,8 @@ modalBtn.forEach((btn) => btn.addEventListener("click", launchModal));
 function launchModal() {
   modalbg.style.display = "block";
 }
+
+//Close modal 
+closeModal.forEach((btn) => btn.addEventListener("click", () => {
+  modalbg.style.display = " none"
+}));
