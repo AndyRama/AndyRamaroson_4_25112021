f9be4d621a532837ad7bff2670882b1b8c50c9ff
AndyRama <andyramaroson@gmail.com>
2022-02-25 17:31:11 +0100

    [enhancement] Validation for lastname

--- modal.js
+++ modal.js
@@ -24,16 +24,30 @@ closeModal.forEach((btn) => btn.addEventListener("click", () => {
 }));
 
 //DOM elements (Global Variables)
+const formModal = document.getElementById("form-modal");
+
 const firstName = document.getElementById("first");
+const lastName = document.getElementById("last");
+
 const errorFirst = document.querySelector(".errorFirst");
-const formModal = document.getElementById("form-modal")
+const errorLast = document.querySelector(".errorLast");
 
 function resetError() {
   errorFirst.innerHTML = "";
+  errorLast.innerHTML = "";
 }
 
-//Check validation for firstname[nb letters] < 2
+//Check validation for firstname[nb letters] > 2
 function checkFirstName(value) {
+  if(value && value.length > 2) {
+    return true;
+  }else {
+    return false;
+  }
+}
+
+//Check validation for Lastname[nb letters] > 2
+function checkLastName(value) {
   if(value && value.length > 2) {
     return true;
   }else {
@@ -53,5 +67,15 @@ formModal.addEventListener("submit", event => {
     errorFirst.innerHTML = "Votre prénom est valide.";
     errorFirst.style.color = "green";
     errorFirst.style.fontSize = "0.8rem";
+  }
+
+  if(!checkLastName(lastName.value)) {
+    errorLast.innerHTML = "Veuillez entrer 2 caractères ou plus pour votre prénom.";
+    errorLast.style.color = "red";
+    errorLast.style.fontSize = "0.8rem";
+  } else {
+    errorLast.innerHTML = "Votre nom est valide.";
+    errorLast.style.color = "green";
+    errorLast.style.fontSize = "0.8rem";
   }
 });
