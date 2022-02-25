86107c3755dd78217ff9cfc692dd5de2ce0d6665
AndyRama <andyramaroson@gmail.com>
2022-02-25 17:18:51 +0100

    [enhancement] Validation for firstname

--- modal.js
+++ modal.js
@@ -22,3 +22,36 @@ modalBtn.forEach((btn) => btn.addEventListener("click",()=> {
 closeModal.forEach((btn) => btn.addEventListener("click", () => {
   modalbg.style.display = " none"
 }));
+
+//DOM elements (Global Variables)
+const firstName = document.getElementById("first");
+const errorFirst = document.querySelector(".errorFirst");
+const formModal = document.getElementById("form-modal")
+
+function resetError() {
+  errorFirst.innerHTML = "";
+}
+
+//Check validation for firstname[nb letters] < 2
+function checkFirstName(value) {
+  if(value && value.length > 2) {
+    return true;
+  }else {
+    return false;
+  }
+}
+
+//listen activity for form
+formModal.addEventListener("submit", event => {
+  event.preventDefault();
+  resetError();
+  if(!checkFirstName(firstName.value)) {
+    errorFirst.innerHTML = "Veuillez entrer 2 caractères ou plus pour votre prénom.";
+    errorFirst.style.color = "red";
+    errorFirst.style.fontSize = "0.8rem";
+  } else {
+    errorFirst.innerHTML = "Votre prénom est valide.";
+    errorFirst.style.color = "green";
+    errorFirst.style.fontSize = "0.8rem";
+  }
+});
